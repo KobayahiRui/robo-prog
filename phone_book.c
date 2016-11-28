91615bfe5ba589d705749279ee4b7169501ed03f
KobayahiRui <roboroborx782@gmail.com>
2016-11-28 19:18:50 +0900

    編集機能追加

--- phone_book.c
+++ phone_book.c
@@ -79,6 +79,8 @@ void Ser(){			//検索モード
 	}
 	fclose(fp);
 }
+
+
 void IDser(){			//ID検索モード
 	char name[50][50],add[50][50];
 	int c=0,t,n,fid=0,l[50],top2[50],pp,ids;
@@ -86,6 +88,7 @@ void IDser(){			//ID検索モード
 
 	fp = fopen("data.txt","r");
 	printf("serch\n");
+	scanf("%*[^0-9]");
 	scanf(" %d",&ids);
 	while((fscanf(fp,"%*s %d//%*s %[^\n//]//%*s %1d%ld//%*s %s",&l[c],name[c],&top2[c],&e[c],add[c]))!=EOF){
 		c++;
@@ -117,6 +120,98 @@ void cl(){		//ファイルデータ削除
 	fclose(fp);
 }
 
+void iddel(){
+	char name[50][50],add[50][50];
+	int c=0,t,n,fid=0,l[50],top2[50],pp,ids,i;
+	long int e[50];
+	fp = fopen("data.txt","r");
+	printf("serch\n");
+	scanf(" %*[^0-9]");
+	scanf(" %d",&ids);
+	printf("#%d\n",ids);
+	while((fscanf(fp,"%*s %d//%*s %[^\n//]//%*s %1d%ld//%*s %s",&l[c],name[c],&top2[c],&e[c],add[c]))!=EOF){
+		c++;
+	}
+	for(t=0;t<c;t++){
+		if(l[t]==ids){
+			printf("find!!\n");
+			fid=1;
+			break;
+		}
+	}
+	if(fid){
+		printf("------------------------------------------------------\n");
+		printf("ID: %d\n",l[t]);
+		printf("name: %s\n",name[t]);
+		printf("phone-number: %d%ld\n",top2[t],e[t]);
+		printf("address: %s\n",add[t]);
+		printf("------------------------------------------------------\n");	
+		printf("delete....\n");
+	}else{
+		printf("not find...\n");
+	}
+	fclose(fp);
+	if(fid){
+		fp =fopen("data.txt","w");	
+		for(i=0;i<c;i++){
+			if(i==t){
+				continue;
+			}
+			if(top2[i]!=-1){
+				fprintf(fp,"ID: %d//name: %s//phone-number: %d%ld//address: %s\n",l[i],name[i],top2[i],e[t],add[t]);
+			}else{
+				fprintf(fp,"ID: %d//name: %s//phone-number: %ld//address: %s\n",l[i],name[i],e[t],add[t]);
+			}	
+		}	
+		fclose(fp);
+	}
+}
+
+void namedel(){			
+	char ser[50],name[50][50],add[50][50];
+	int c=0,t,n,fid=0,l[50],top2[50],pp,i;
+	long int e[50];
+	fp = fopen("data.txt","r");
+	printf("serch\n");
+	scanf(" %50[^\n]",ser);
+	while((fscanf(fp,"%*s %d//%*s %[^\n//]//%*s %1d%ld//%*s %s",&l[c],name[c],&top2[c],&e[c],add[c]))!=EOF){
+		c++;
+	}
+	for(t=0;t<=c;t++){
+		printf("%s\n",name[t]);
+		if(!strcmp(name[t],ser)){
+			printf("found!!\n");
+			fid=1;
+			break;
+		}
+	}
+	fclose(fp);
+	if(fid){
+		printf("------------------------------------------------------\n");
+		printf("ID: %d\n",l[t]);
+		printf("name: %s\n",name[t]);
+		printf("phone-number: %d%ld\n",top2[t],e[t]);
+		printf("address: %s\n",add[t]); 
+		printf("------------------------------------------------------\n");
+	}else{
+		printf("not found...\n");
+	}
+	if(fid){
+		fp =fopen("data.txt","w");	
+		for(i=0;i<c;i++){
+			if(i==t){
+				continue;
+			}
+			if(top2[i]!=-1){
+				fprintf(fp,"ID: %d//name: %s//phone-number: %d%ld//address: %s\n",l[i],name[i],top2[i],e[t],add[t]);
+			}else{
+				fprintf(fp,"ID: %d//name: %s//phone-number: %ld//address: %s\n",l[i],name[i],e[t],add[t]);
+			}	
+		}	
+		fclose(fp);
+	}
+}
+
 void list(){		//ファイル内データリスト	
 	char name[50][50],add[50][50];
 	int c=0,t,l[50],top2[50];
@@ -139,8 +234,8 @@ void list(){		//ファイル内データリスト
 int main(){
 	while(1){
 		char mode;
-		printf("mode select Registration:r Search:s IDserch:i list:l clear:c end:q\n");
-		scanf(" %[lrscqi]",&mode);
+		printf("mode select Registration:r Search:s IDserch:i IDdelete:d list:l clear:c end:q\n");
+		scanf(" %[Ddlrscqi]",&mode);
 		switch(mode){
 			case 'r':
 				Regi();
@@ -150,6 +245,12 @@ int main(){
 				break;
 			case 'i':
 				IDser();
+				break;
+			case 'd':
+				iddel();
+				break;
+			case 'D':
+				namedel();
 				break;
 			case 'l':
 				list();
